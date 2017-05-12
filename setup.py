58357e85b15115fbae6c9111e743de5797d0e89b
Mick Koch <mick@kochm.co>
2017-05-12 13:33:41 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="asyncinit",
-    version="0.1.0",
+    version="0.2.0",
     description="Class decorator to enable async __init__",
     author="Mick Koch",
     author_email="mick@kochm.co",
